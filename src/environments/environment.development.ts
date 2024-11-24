e65a44931ab07941faabe003fea099a5e072424a
Islam Kordy <ikordy@master-works.sa>
2024-11-24 15:59:57 +0200

    fix proxy settings

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  proxyBase: 'http://localhost:5030/api/v1',
-  baseURL: 'http://localhost:5030/api/v1',
+  proxyBase: '/api',
+  baseURL: 'https://fotrahbet.ripc.gov.sa/api/v1',
   tahakomUrl: 'https://fotrahbe.ripc.gov.sa/api/v1',
   redirectURL: location.origin + '/public/payment-success',
   responseURL: location.origin + '/public/payment-success',
